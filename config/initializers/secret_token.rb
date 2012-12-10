b4ea28b0b1e4bfcc4bc4eb9a3a6df3afeb011f4e
tarik Filali <tarik.filali@gmail.com>
2012-12-10 14:33:39 +0100

    Depot Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimelessTracker::Application.config.secret_token = '3a92a0837fd1f89d90f9af0832559130981bb772a871a2a76280d7b4e28621bad58dfcabad123f005b0b0f65851c5025feb2a91568eb395635354767ad8406fe'
